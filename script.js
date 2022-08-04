7f81501f036e8c3dbce0332857d439fe898037b6
Fluke <hunterx52110@gmail.com>
2022-08-04 14:13:14 +0700

    completed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const divUserCard = document.getElementById("div-user-card");
+const btnRandom = document.querySelector("#btn-random");
+const divLoadingCard = document.getElementById("div-loading-card");
+const spanGenderIcon = document.querySelector("#btn-random");
+
+Element.style.display = "none";
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+Element.style.display = " ";
